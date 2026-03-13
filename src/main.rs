ccca5d54ba37a6a1453ae79b940d3cff4788da94
DimaT <dimalevenko6@gmail.com>
2026-03-13 10:30:35 +0200

    const

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,29 @@ use rayon::prelude::*;
 use std::path::Path;
 
 fn main() {
+    const MASK_DARK: [[u8; 3]; 3] = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+    ];
+
+    const MASK_GRAY: [[u8; 3]; 3] = [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+    ];
+
+    const MASK_LIGHT: [[u8; 3]; 3] = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0],
+    ];
+
+    const MASK_WHITE: [[u8; 3]; 3] = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+    ];
     let img = Path::new("img.png");
     let img = open(img).expect("error img");
     let width = img.width();
