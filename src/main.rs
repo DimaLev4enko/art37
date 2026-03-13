508525ca2fcd6a2cbd90ee392737b9c928293ac2
Dima <dimalevenko6@gmail.com>
2026-03-13 13:29:32 +0200

    txt version

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use image::open;
 use rayon::prelude::*;
 use std::path::Path;
 
+const MASK_DARK: [[u8; 3]; 3] = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+
+const MASK_GRAY: [[u8; 3]; 3] = [[0, 1, 0], [1, 1, 1], [0, 1, 0]];
+
+const MASK_LIGHT: [[u8; 3]; 3] = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
+
+const MASK_WHITE: [[u8; 3]; 3] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 fn main() {
-    const MASK_DARK: [[u8; 3]; 3] = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
-
-    const MASK_GRAY: [[u8; 3]; 3] = [[0, 1, 0], [1, 1, 1], [0, 1, 0]];
-
-    const MASK_LIGHT: [[u8; 3]; 3] = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
-
-    const MASK_WHITE: [[u8; 3]; 3] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     let img = Path::new("img.png");
     let img = open(img).expect("error img");
     let width = img.width();
@@ -28,6 +28,63 @@ fn main() {
     image::save_buffer(path, &res, width, height, image::ColorType::L8).expect("neydacha");
     let width = ((width * 3) + 1) as usize;
     let height = (height * 3) as usize;
-    let mut buffer = vec![b" "; width * height];
-    buffer.par_chunks_mut(width);
+    let mut buffer = vec![b' '; width * height];
+    buffer
+        .par_chunks_mut(width)
+        .enumerate()
+        .for_each(|(y, row)| {
+            let rowy = y / 3;
+            let maty = y % 3;
+            let mut currtect = b'3';
+            for x in 0..(width - 1) {
+                let pixel = x / 3;
+                let matx = x % 3;
+                let cords = (rowy * (width / 3)) + pixel;
+                match res[cords] {
+                    0..64 => {
+                        if MASK_DARK[maty][matx] == 1 {
+                            row[x] = currtect;
+                            if currtect == b'3' {
+                                currtect = b'7';
+                            } else if currtect == b'7' {
+                                currtect = b'3';
+                            }
+                        }
+                    }
+                    64..128 => {
+                        if MASK_GRAY[maty][matx] == 1 {
+                            row[x] = currtect;
+                            if currtect == b'3' {
+                                currtect = b'7';
+                            } else if currtect == b'7' {
+                                currtect = b'3';
+                            }
+                        }
+                    }
+                    128..192 => {
+                        if MASK_LIGHT[maty][matx] == 1 {
+                            row[x] = currtect;
+                            if currtect == b'3' {
+                                currtect = b'7';
+                            } else if currtect == b'7' {
+                                currtect = b'3';
+                            }
+                        }
+                    }
+                    192..=255 => {
+                        if MASK_WHITE[maty][matx] == 1 {
+                            row[x] = currtect;
+                            if currtect == b'3' {
+                                currtect = b'7';
+                            } else if currtect == b'7' {
+                                currtect = b'3';
+                            }
+                        }
+                    }
+                }
+            }
+            row[width - 1] = b'\n';
+        });
+    std::fs::write("output.txt", &buffer).expect("Не удалось записать файл");
+    println!("Готово! Смотри результат в output.txt");
 }
